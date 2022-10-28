da2134770b210a9db981819e96e323be7acbadbd
Jeffry1990 <109137757+Jeffry1990@users.noreply.github.com>
2022-10-28 15:11:11 +0300

    Add files via upload

---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+            char openingBracket = '(';
+            char closingBracket = ')';
+            int currentDepth = 0;
+            int maxDepth = 0;
+
+            Console.Write("Введите скобочное выражение из круглых скобок: ");
+            userInput = Console.ReadLine();
+
+            foreach (var symbol in userInput)
+            {
+                if (symbol == openingBracket)
+                {
+                    currentDepth++;
+
+                    if (currentDepth > maxDepth)
+                    {
+                        maxDepth = currentDepth;
+                    }
+                }
+                else if (symbol == closingBracket)
+                {
+                    currentDepth--;
+
+                    if (currentDepth < 0)
+                    {
+                        Console.WriteLine("Скобочное выражение некоректно.");
+                        break;
+                    }
+                }
+            }
+
+            if (currentDepth == 0)
+            {
+                Console.WriteLine($"Скобочное выражение корректно.\nЕго максимальная глубина {maxDepth}");
+            }
+            else if (currentDepth > 0)
+            {
+                Console.WriteLine("Скобочное выражение некоректно.");
+            }
+        }
+    }
+}
+
